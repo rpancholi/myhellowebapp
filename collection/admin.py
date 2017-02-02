c67147b0c2500cce852609872ce4c09b22db92d6
Rupesh Pancholi <rpancholi15@gmail.com>
2017-02-01 22:33:40 -0600

    Chapter 7
    Dynamic Data

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from collection.models import Places
+
+class PlacesAdmin(admin.ModelAdmin):
+	model = Places
+	list_display = ('name', 'description',)
+	prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Places, PlacesAdmin)
